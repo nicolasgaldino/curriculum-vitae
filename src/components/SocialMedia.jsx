6c98d585fc742b40b57961cf5712477c02e84714
nicolasgaldino <nicolasesmael1998@gmail.com>
2022-07-15 19:02:05 -0300

    Componente de Redes Sociais criado.

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,9 +1,30 @@
 import { FaLinkedin, FaGithub, FaInstagram, FaTwitter } from 'react-icons/fa';
 import styles from '../styles/components/SocialMedia.module.css';
 
+const socialNetworks = [
+  { name: "linkedin", link: "https://www.linkedin.com/in/nícolas-galdino-esmael-8370ab199", icon: <FaLinkedin /> },
+  { name: "github", link: "https://github.com/nicolasgaldino", icon: <FaGithub /> },
+  { name: "twitter", link: "https://twitter.com/galdino_esmael", icon: <FaTwitter /> },
+  { name: "instagram", link: "https://www.instagram.com/galdino_esmael/", icon: <FaInstagram /> },
+];
+
 const SocialMedia = () => {
   return (
-    <div>SocialMedia</div>
+    <>
+      <section className={styles.socialNetworks}>
+        {socialNetworks.map((item) => (
+          <a
+          href={item.link}
+          className={styles.socialBtn}
+          id={item.name}
+          target="_blank"
+          rel="noreferrer"
+          >
+            {item.icon}
+          </a>
+        ))}
+      </section>
+    </>
   )
 }
 
